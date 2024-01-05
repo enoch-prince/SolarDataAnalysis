094384c01c04162a47051e14533179641480724e
enoch-prince <princealva11@gmail.com>
2024-01-05 15:51:17 +0000

    Added fixes for dealing with non numeric data

--- cleanup_data.py
+++ cleanup_data.py
@@ -1,10 +1,12 @@
+from datetime import time
 from pathlib import Path
 import argparse
 import sys
 from typing import Generator
 import pandas as pd
 
-DEBUG = True
+import utils
+
 columns_to_drop = ["Panel Ambient(°C)", "Floor Ambient(°C)", "Sys Temp(°C)", "Sys Hum(%)", "Vbat(V)"]
 row_start = 173
 row_end = 308
@@ -19,63 +21,75 @@ def parseCmdLineForCSVPath() -> str:
         '--log', default=sys.stdout, type=argparse.FileType('w'),
         help='the file where the logs should be written')
     args = parser.parse_args()
-    if DEBUG:
+    if utils.DEBUG:
         args.log.write('File path given %s\n' % args.csv_folder_path)
     return args.csv_folder_path
 
-
-def resolveDataFolderPaths(path_str: str):
-    path = Path(path_str)
-
-    path_exists = path.exists()
-    if DEBUG:
-        print("\nDoes path exist: ", path_exists, "\n")
-
-    if path_exists:
-        return path.glob("**")
-
-def resolveCSVFilePaths(folder_path: Path) -> Generator[Path, None, None] | None:
-    return folder_path.glob("*.csv")
-
-
 def processCSVFiles(folder_name:str, csv_glob: Generator[Path, None, None]):
     for csv_path in csv_glob:
-        if DEBUG:
+        if folder_name is csv_path.name:
+            continue
+        if utils.DEBUG:
             print("Processing file:", csv_path.resolve())
         df = pd.read_csv(csv_path)
-        if DEBUG:
+
+        if utils.DEBUG:
             print("Dropping columns specified......\n")
         columns_to_drop_exists = all([True if column in df.columns else False for column in columns_to_drop])
         if not columns_to_drop_exists:
-            if DEBUG:
+            if utils.DEBUG:
                 print(f"Skipping file {csv_path.resolve()} because columns: {columns_to_drop} does not exist in file")
             continue
         
         df.drop(columns_to_drop, axis=1, inplace=True)
 
-        if DEBUG:
+        if utils.DEBUG:
             print("Selecting data range from 9am - 4pm .......\n")
-        df = df.iloc[row_start:row_end]
-        if DEBUG:
-            print("Writing modified data to another file in the /Data in the CWD...\n")
+
+        mask = ((df["Started"].values >= '09:00:00') & (df["Started"].values <= '16:05:00'))
+        df = df.loc[mask]
         
+        iv_column_names = [column for column in df.columns if "IV" in column]
+        for column in iv_column_names:
+            splitIVdataIntoSeperateColumns(df, column)
+        
+        if utils.DEBUG:
+            print("Writing modified data to another file in the /Data in the CWD...\n")
+
         # Save Modified Data
         df.to_csv(f"Data/{folder_name}/{csv_path.name}", index=False)
-        if DEBUG:
+        if utils.DEBUG:
             print(f"Data/{folder_name}/{csv_path.name} DONE!!!\n--------------\n")
     
 
+def splitIVdataIntoSeperateColumns(df: pd.DataFrame, column_name: str):
+    iv_data_str_list = df[column_name].to_list()
+
+    v_list = []
+    i_list = []
+    iv_pair_float_list = [[float(data.split(":")[0]), float(data.split(":")[1])]  for data in iv_data_str_list]
+
+    for iv_pair in iv_pair_float_list:
+        v_list.append(iv_pair[0])
+        i_list.append(iv_pair[1])
+    column_index = df.columns.tolist().index(column_name)
+    
+    df.insert(column_index, f"V{column_name[2:]}", v_list)
+    df.insert(column_index+1, f"I{column_name[2:]}", i_list)
+    df.drop([column_name], axis=1, inplace=True)
 
 def main():
     path_str = parseCmdLineForCSVPath()
-    folder_glob = resolveDataFolderPaths(path_str)
+    folder_name, folder_glob = utils.resolveDataFolderPaths(path_str)
     
     if folder_glob is not None:
         for folder_path in folder_glob:
+            if folder_name is folder_path.name:
+                continue
             new_path = Path(f"Data/{folder_path.name}")
             if not new_path.exists():
                 new_path.mkdir()
-            csv_file_glob = resolveCSVFilePaths(folder_path)
+            csv_file_glob = utils.resolveCSVFilePaths(folder_path)
             if csv_file_glob is None:
                 continue
             processCSVFiles(folder_path.name, csv_file_glob)
